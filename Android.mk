d98a607917a226da413fab267580fbcc94c48713
Surge Raval <Surge1223@gmail.com>
2018-02-16 23:43:29 +0000

    fix some refs

--- Android.mk
+++ Android.mk
@@ -22,11 +22,11 @@ LOCAL_CFLAGS += -DFASTBOOT_VERSION="\"$(tool_version)\""
 LOCAL_CPPFLAGS += -std=gnu++1y
 
 LOCAL_C_INCLUDES := \
-  $(LOCAL_PATH)/../adb \
-  $(LOCAL_PATH)/../mkbootimg \
-  $(LOCAL_PATH)/../../extras/f2fs_utils \
+  $(LOCAL_PATH)/system/core/adb \
+  $(LOCAL_PATH)/system/core/mkbootimg \
+  $(LOCAL_PATH)/system/extras/f2fs_utils \
   $(LOCAL_PATH)/include \
-  system/core/include \
+  $(LOCAL_PATH)/system/core/include \
 
 LOCAL_SRC_FILES := \
     bootimg_utils.cpp \
@@ -38,13 +38,10 @@ LOCAL_SRC_FILES := \
     tcp.cpp \
     udp.cpp \
     util.cpp \
+    usb_linux.cpp
 
 LOCAL_MODULE_TAGS := debug
-LOCAL_CFLAGS += -Wall -Wextra -Werror -Wunreachable-code
-LOCAL_REQUIRED_MODULES := mke2fs e2fsdroid
-
-LOCAL_SRC_FILES += usb_linux.cpp
-LOCAL_STATIC_LIBRARIES += libselinux
+LOCAL_CFLAGS += -Wall -Wextra -Werror -Wunreachable-code -DUSE_F2FS
 
 LOCAL_STATIC_LIBRARIES += \
     libziparchive \
@@ -56,17 +53,12 @@ LOCAL_STATIC_LIBRARIES += \
     libdiagnose_usb \
     libbase \
     libcutils \
-    libutils \
+    libselinux \
+    libf2fs_utils_host \
+    libf2fs_ioutils_host \
+    libf2fs_dlutils_host
 
-# libf2fs_dlutils_host will dlopen("libf2fs_fmt_host_dyn")
-LOCAL_CFLAGS += -DUSE_F2FS
-LOCAL_LDFLAGS_linux := -ldl -rdynamic -Wl,-rpath,.
-LOCAL_REQUIRED_MODULES_linux := libf2fs_fmt_host_dyn
-# The following libf2fs_* are from system/extras/f2fs_utils,
-# and do not use code in external/f2fs-tools.
-LOCAL_STATIC_LIBRARIES += libf2fs_utils_host libf2fs_ioutils_host libf2fs_dlutils_host
-
-LOCAL_CXX_STL := libc++
+#LOCAL_CXX_STL := libc++
 LOCAL_MODULE := fastboot
 LOCAL_MODULE_STEM_32 := fastboot
 LOCAL_MODULE_STEM_64 := fastboot64
@@ -77,17 +69,14 @@ include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := libdiagnose_usb
-LOCAL_CFLAGS := $(LIBADB_CFLAGS)
 LOCAL_SRC_FILES := diagnose_usb.cpp
-# Even though we're building a static library (and thus there's no link step for 
-# this to take effect), this adds the includes to our path.
 
 LOCAL_C_INCLUDES := \
-  $(LOCAL_PATH)/../adb \
-  $(LOCAL_PATH)/../mkbootimg \
-  $(LOCAL_PATH)/../../extras/f2fs_utils \
+  $(LOCAL_PATH)/system/core/adb \
+  $(LOCAL_PATH)/system/core/mkbootimg \
+  $(LOCAL_PATH)/system/extras/f2fs_utils \
   $(LOCAL_PATH)/include \
-  system/core/include
+  $(LOCAL_PATH)/system/core/include
 
 LOCAL_STATIC_LIBRARIES += libbase
 include $(BUILD_STATIC_LIBRARY)
